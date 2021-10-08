0eeb520cc898939fc129f68d14953a8314265cad
Christine Ong <christine_ong@ucsb.edu>
2021-10-08 15:14:48 -0700

    fix

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World App
+          Hello World!
         </p>
         <a
           className="App-link"
